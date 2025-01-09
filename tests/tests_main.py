df36d6f70b327080291d014a464408ac83b904bd
Maximo Hardaker <MaximoHardaker@Natashas-MacBook-Air.local>
2025-01-09 15:59:50 +0000

    first unittest

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -1,23 +1,15 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
+import unittest
+import pandas as pd
+from main import calculate_gdp_growth
 
-# Filter only BRICS countries from the final data
-brics_final_data = final_brics_g7_data[final_brics_g7_data['Country'].isin(brics_countries)]
+class TestGDPAnalysis(unittest.TestCase):
 
-# Set the plot style
-sns.set(style="whitegrid")
+    def setUp(self):
+        self.data = pd.read_csv("data/gdp_per_capita.csv")
+        self.data.rename(columns={'Entity': 'Country', 
+                                  'GDP per capita, PPP (constant 2017 international $)': 'GDP/Capita (PPP)'}, inplace=True)
+        self.brics_countries = ['Brazil', 'Russia', 'India', 'China', 'South Africa']
+        self.g7_countries = ['Canada', 'France', 'Germany', 'Italy', 'Japan', 'United Kingdom', 'United States']
 
-# Create the plot
-plt.figure(figsize=(10, 6))
-sns.lineplot(data=brics_final_data, x='Year', y='GDP/Capita (PPP)', hue='Country', marker='o')
-
-# Add titles and labels
-plt.title("GDP/Capita (PPP) for BRICS Countries (2000-2020)", fontsize=16)
-plt.xlabel("Year", fontsize=14)
-plt.ylabel("GDP/Capita (PPP)", fontsize=14)
-plt.xticks([2000, 2010, 2020], fontsize=12)
-plt.legend(title="Country", fontsize=10)
-plt.tight_layout()
-
-# Display the plot
-plt.show()
+if __name__ == "__main__":
+    unittest.main()
