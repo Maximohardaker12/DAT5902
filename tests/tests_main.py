ceb94cd4b1bd103600c305f32e09ac281e7a1c54
Maximohardaker12 <delimamaximo12@gmail.com>
2025-01-10 16:24:38 +0000

    Update tests_main.py

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -1,10 +1,6 @@
 import unittest
 import pandas as pd
 
-<<<<<<< HEAD
-=======
-
->>>>>>> c6fa700015b93f751c589b9cff673a6e39dd9f11
 from main.main import calculate_gdp_growth  
 
 class TestGDPAnalysis(unittest.TestCase):
